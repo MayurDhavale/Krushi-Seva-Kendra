2a13b5a79394119224e658e9cc1afdeca0525003
Mayur Dhavale <mayurdhavale2002@gmail.com>
2026-06-30 22:31:43 +0530

    feat(auth): implement user registration service

--- backend/src/main/java/com/krushisevakendra/billing/auth/service/AuthServiceImpl.java
+++ backend/src/main/java/com/krushisevakendra/billing/auth/service/AuthServiceImpl.java
@@ -1,9 +1,13 @@
 package com.krushisevakendra.billing.auth.service;
 
 import com.krushisevakendra.billing.auth.dto.RegisterRequest;
+import com.krushisevakendra.billing.auth.entity.Role;
+import com.krushisevakendra.billing.auth.entity.User;
 import com.krushisevakendra.billing.auth.repository.UserRepository;
 import com.krushisevakendra.billing.common.response.ApiResponse;
+import com.krushisevakendra.billing.exception.DuplicateResourceException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,8 +17,32 @@ public class AuthServiceImpl implements AuthService{
 
     private final UserRepository userRepository;
 
+    private final PasswordEncoder passwordEncoder;
+
     @Override
     public ApiResponse<String> register(RegisterRequest request) {
-        return ApiResponse.success("Registration logic will be implemented in the Next Step.",null);
+
+        if(userRepository.existsByUsername(request.getUsername())){
+            throw new DuplicateResourceException("Username already exist");
+        }
+
+        if(userRepository.existsByEmail(request.getEmail())){
+            throw new DuplicateResourceException("Email already exist");
+        }
+
+        User user = User.builder()
+                .name(request.getFullName())
+                .username(request.getUsername())
+                .email(request.getEmail())
+                .mobileNumber(request.getMobileNumber())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.ADMIN)
+                .enabled(true)
+                .build();
+
+        userRepository.save(user);
+
+        return ApiResponse.success("User registered successfully", null);
+
     }
 }
