a479a7a26eda85e4983c0818db2004472f9ea43b
Mayur Dhavale <mayurdhavale2002@gmail.com>
2026-06-30 22:09:24 +0530

    feat(auth): add authentication service layer

--- backend/src/main/java/com/krushisevakendra/billing/auth/service/AuthService.java
+++ backend/src/main/java/com/krushisevakendra/billing/auth/service/AuthService.java
@@ -1,4 +1,9 @@
 package com.krushisevakendra.billing.auth.service;
 
-public class AuthService {
+import com.krushisevakendra.billing.auth.dto.RegisterRequest;
+import com.krushisevakendra.billing.common.response.ApiResponse;
+
+public interface AuthService {
+
+    ApiResponse<String> register(RegisterRequest request);
 }
--- backend/src/main/java/com/krushisevakendra/billing/auth/service/AuthServiceImpl.java
+++ backend/src/main/java/com/krushisevakendra/billing/auth/service/AuthServiceImpl.java
@@ -1,4 +1,20 @@
 package com.krushisevakendra.billing.auth.service;
 
-public class AuthServiceImpl {
+import com.krushisevakendra.billing.auth.dto.RegisterRequest;
+import com.krushisevakendra.billing.auth.repository.UserRepository;
+import com.krushisevakendra.billing.common.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+
+public class AuthServiceImpl implements AuthService{
+
+    private final UserRepository userRepository;
+
+    @Override
+    public ApiResponse<String> register(RegisterRequest request) {
+        return ApiResponse.success("Registration logic will be implemented in the Next Step.",null);
+    }
 }
