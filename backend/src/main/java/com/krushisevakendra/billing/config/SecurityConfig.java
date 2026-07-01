cc255ebcff1dc7165cec92a4311518dc9481b769
Mayur Dhavale <mayurdhavale2002@gmail.com>
2026-07-01 22:59:34 +0530

    feat(auth): configure stateless Spring Security

--- backend/src/main/java/com/krushisevakendra/billing/config/SecurityConfig.java
+++ backend/src/main/java/com/krushisevakendra/billing/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.krushisevakendra.billing.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,9 +15,34 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-                .csrf(csrf -> csrf.disable())
+                //Disable CSRF for REST APIS
+                .csrf(csrf->csrf.disable())
+
+                //Stateless session (required for JWT)
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                //Authorization Rule
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll())
+
+                        //Authentication APIs
+                                .requestMatchers(HttpMethod.POST,
+                                        "/api/v1/auth/register",
+                                        "/api/v1/auth/login")
+                                .permitAll()
+
+                        //Swagger
+                                .requestMatchers(
+                                        "/swagger-ui/**",
+                                        "/v3/api-docs/**")
+                                .permitAll()
+
+                        //Everything Else
+                                .anyRequest()
+                                .authenticated()
+                )
+
+                // Temporary Basic Auth
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
